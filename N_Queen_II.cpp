cd1bc7b305ecf91062458a3d7059270a9a01690a
Yash Parmar <yashparmar15700@gmail.com>
2020-06-30 21:58:07 +0530

    N_Queen_II

---
+++ N_Queen_II.cpp
@@ -0,0 +1,61 @@
+// The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+
+
+// Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+
+class Solution {
+public:
+    bool check(int i , int j , vector<pair<int,int>> positions){
+        bool flag = true;
+        for(int k = 0 ; k < positions.size() ; k++){
+            int a = positions[k].first;
+            int b = positions[k].second;
+            if((a == i) or (b == j) or ((a + b) == (i + j)) or ((a - b) == (i - j))){
+                return false;
+            }
+        }
+        return true;
+    }
+    void recur(int n , vector<pair<int,int>> positions , vector<vector<string>> &ans , int count){
+        if(count >= n){
+            
+            vector<string> sol;
+            for(int i = 0 ; i < n ; i++){
+                string temp;
+                for(int j = 0 ; j < n ; j++){
+                    bool flag = 0;
+                    for(int k = 0 ; k < positions.size() ; k++){
+                        if(positions[k].first == i and positions[k].second == j){
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if(flag)
+                        temp = temp + "Q";
+                    else
+                        temp = temp + ".";
+                }
+                sol.push_back(temp);
+            }
+            ans.push_back(sol);
+            return;
+        }
+
+            for(int j = 0 ; j < n ; j++){
+                if(check(count,j,positions)){
+                    positions.push_back(make_pair(count,j));
+                    recur(n,positions,ans,count + 1);
+                    positions.pop_back();
+                }
+            }
+    }
+    int totalNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<pair<int,int>> positions;
+        int count = 0;
+        recur(n,positions,ans,count);
+        return ans.size();
+    }
+};
