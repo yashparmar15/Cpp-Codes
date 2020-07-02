d601eb51a1f244de79e0eba51f0010f5eef71960
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-07-03 00:13:02 +0530

    Count Complete Tree Nodes

---
+++ Count_Complete_Tree_Nodes.cpp
@@ -0,0 +1,46 @@
+// Given a complete binary tree, count the number of nodes.
+
+// Note:
+
+// Definition of a complete binary tree from Wikipedia:
+// In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+// Example:
+
+// Input: 
+//     1
+//    / \
+//   2   3
+//  / \  /
+// 4  5 6
+
+// Output: 6
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void recur(TreeNode* root , int &ans){
+        if(root == NULL)
+            return;
+        ans++;
+        recur(root->left,ans);
+        recur(root->right,ans);
+    }
+    int countNodes(TreeNode* root) {
+        int ans = 0;
+        recur(root,ans);
+        return ans;
+    }
+};
