6d61bd6f8f9b5726b9486ebbc1923772bdb0e92b
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-07-07 22:58:27 +0530

    Count and Say

---
+++ Count_and_Say.cpp
@@ -0,0 +1,38 @@
+// The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
+// 1 is read off as "one 1" or 11.
+// 11 is read off as "two 1s" or 21.
+// 21 is read off as "one 2, then one 1" or 1211.
+
+// Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit.
+
+// Note: Each term of the sequence of integers will be represented as a string.
+
+ 
+
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 0) return "";
+        string res = "1";
+        while (--n) {
+            string cur = "";
+            for (int i = 0; i < res.size(); i++) {
+                int count = 1;
+                 while ((i + 1 < res.size()) && (res[i] == res[i + 1])){
+                    count++;    
+                    i++;
+                }
+                cur += to_string(count) + res[i];
+            }
+            res = cur;
+        }
+        return res;
+    }
+};
