e9c0ddae3db98654c529eb0455cafdc7632b5042
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-07-09 22:29:04 +0530

    Binary Tree Right Side View

---
+++ Binary_Tree_Right_Side_View.cpp
@@ -0,0 +1,55 @@
+// Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+// Example:
+
+// Input: [1,2,3,null,5,null,4]
+// Output: [1, 3, 4]
+// Explanation:
+
+//    1            <---
+//  /   \
+// 2     3         <---
+//  \     \
+//   5     4       <---
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        queue<TreeNode*> q;
+        if(!root)
+            return ans;
+        q.push(root);
+        int s = q.size();
+        while(!q.empty()){
+            s = q.size();
+            if(s)
+                ans.push_back(0);
+            int S = ans.size() - 1;
+            while(s--){
+                TreeNode *temp = q.front();
+                ans[S] = temp->val;
+                q.pop();
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+            }
+        }
+        return ans;
+    }
+};
