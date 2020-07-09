7c9805997673dcf28537f93f23ab08d0293288e1
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-07-09 22:22:38 +0530

    Polpulating Next Right Pointers in Each Node

---
+++ Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,68 @@
+// You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+// struct Node {
+//   int val;
+//   Node *left;
+//   Node *right;
+//   Node *next;
+// }
+// Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+// Initially, all next pointers are set to NULL.
+
+ 
+
+// Follow up:
+
+// You may only use constant extra space.
+// Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+
+
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root)
+            return root;
+        queue<Node*> q;
+        q.push(root);
+        int s = q.size();
+        while(!q.empty()){
+            s = q.size();
+            vector<Node*> temp;
+            while(s--){
+                Node* t = q.front();
+                q.pop();
+                temp.push_back(t);
+                if(t->left != NULL)
+                    q.push(t->left);
+                if(t->right != NULL)
+                    q.push(t->right);
+            }
+            for(int i = 0 ; i < temp.size() - 1 ; i++){
+                temp[i]->next = temp[i + 1];
+            }
+            temp[temp.size() - 1]->next = NULL;
+        }
+        return root;
+    }
+};
