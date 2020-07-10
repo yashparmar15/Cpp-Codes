2dfa0b443631b7e93ef34913541fbfad6abee8c9
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-07-10 21:33:05 +0530

    Find Bottom Left Tree Value

---
+++ Find_Bottom_Left_Tree_Value.cpp
@@ -0,0 +1,64 @@
+// Given a binary tree, find the leftmost value in the last row of the tree.
+
+// Example 1:
+// Input:
+
+//     2
+//    / \
+//   1   3
+
+// Output:
+// 1
+// Example 2:
+// Input:
+
+//         1
+//        / \
+//       2   3
+//      /   / \
+//     4   5   6
+//        /
+//       7
+
+// Output:
+// 7
+// Note: You may assume the tree (i.e., the given root node) is not NULL.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        int ans = 0;
+        if(root == NULL)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int s = q.size();
+        ans = root->val;
+        while(!q.empty()){
+            s = q.size();
+            ans = q.front()->val;
+            while(s--){
+                TreeNode *temp = q.front();
+                q.pop();
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+            }
+        }
+        return ans;
+    }
+};
