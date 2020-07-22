3833a74ba2b62bbb6dcaae104d2b74ff6a06b77c
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-07-22 22:59:46 +0530

    Omkar and Completion

---
+++ Omkar_and_Completion.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n ;
+		cin >> n;
+		for(int i = 0 ; i < n - 1 ; i++){
+			cout << 1 << " ";
+		}
+		cout << "1\n";
+	}
+}
