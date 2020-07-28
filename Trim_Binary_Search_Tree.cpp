4d31b42e089264cc5705c1cc80c1507e4276b5ae
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-07-28 23:44:40 +0530

    Create Trim_Binary_Search_Tree.cpp

---
+++ Trim_Binary_Search_Tree.cpp
@@ -0,0 +1,42 @@
+/*Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+
+Example 1:
+Input: 
+    1
+   / \
+  0   2
+
+  L = 1
+  R = 2
+
+Output: 
+    1
+      \
+       2*/
+       
+       
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) {
+        if(!root)
+            return root;
+        if(root->val < L)
+            return trimBST(root->right,L,R);
+        if(root->val > R)
+            return trimBST(root->left,L,R);
+        root->left = trimBST(root->left,L,R);
+        root->right = trimBST(root->right,L,R);
+        return root;
+    }
+};
