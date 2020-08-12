be8fe63129b8eb89b78c492c67574c7d18f5c750
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-08-12 01:29:01 -0700

    Create Shuffle_an_Array.cpp

---
+++ Shuffle_an_Array.cpp
@@ -0,0 +1,51 @@
+/*Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();*/
+
+
+
+
+class Solution {
+public:
+    vector<int> nums , original;
+    int count = 0;
+    Solution(vector<int>& nums) {
+        this->nums = nums;
+        original = nums;
+        count = 0;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return original;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        if(count == nums.size()){
+            nums = original;
+        }
+        next_permutation(nums.begin(),nums.end());
+        return nums;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
