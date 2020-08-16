3358ef7a8907dee3814b2c5e4fba5a1f6742d3c1
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-08-17 00:12:55 +0530

    Create The_K_Weakest_Rows_in_a_Matrix.cpp

---
+++ The_K_Weakest_Rows_in_a_Matrix.cpp
@@ -0,0 +1,81 @@
+/*Given a m * n matrix mat of ones (representing soldiers) and zeros (representing civilians), return the indexes of the k weakest rows in the matrix ordered from the weakest to the strongest.
+
+A row i is weaker than row j, if the number of soldiers in row i is less than the number of soldiers in row j, or they have the same number of soldiers but i is less than j. Soldiers are always stand in the frontier of a row, that is, always ones may appear first and then zeros.
+
+ 
+
+Example 1:
+
+Input: mat = 
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]], 
+k = 3
+Output: [2,0,3]
+Explanation: 
+The number of soldiers for each row is: 
+row 0 -> 2 
+row 1 -> 4 
+row 2 -> 1 
+row 3 -> 2 
+row 4 -> 5 
+Rows ordered from the weakest to the strongest are [2,0,3,1,4]
+Example 2:
+
+Input: mat = 
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]], 
+k = 2
+Output: [0,2]
+Explanation: 
+The number of soldiers for each row is: 
+row 0 -> 1 
+row 1 -> 4 
+row 2 -> 1 
+row 3 -> 1 
+Rows ordered from the weakest to the strongest are [0,2,3,1]
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+2 <= n, m <= 100
+1 <= k <= m
+matrix[i][j] is either 0 or 1.*/
+
+class Solution {
+public:
+    
+    static bool comp(pair<int,pair<int,int>> a , pair<int,pair<int,int>> b){
+        if(a.second.first == b.second.first)
+            return a.first < b.first;
+        return a.second.first < b.second.first;
+    }
+    
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        unordered_map<int,pair<int,int>> M;
+        for(int i = 0 ; i < mat.size() ; i++){
+            for(int j = 0 ; j < mat[0].size() ; j++){
+                if(mat[i][j] == 1)
+                    M[i].first++;
+                else
+                    M[i].second++;
+            }
+        }
+        vector<pair<int,pair<int,int>>> v;
+        for(auto m : M){
+            v.push_back(make_pair(m.first,make_pair(m.second.first,m.second.second)));
+        }
+        sort(v.begin(),v.end(),comp);
+        vector<int> ans;
+        for(int i = 0 ; i < k ; i++){
+            ans.push_back(v[i].first);
+        }
+        return ans;
+    }
+};
