9105a9889b1a3bf6a2e26fa05a3a76a6a21a1bd7
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-08-21 15:23:17 +0530

    Create Friends_of_Approprite_Ages.cpp

---
+++ Friends_of_Approprite_Ages.cpp
@@ -0,0 +1,59 @@
+/*Some people will make friend requests. The list of their ages is given and ages[i] is the age of the ith person. 
+
+Person A will NOT friend request person B (B != A) if any of the following conditions are true:
+
+age[B] <= 0.5 * age[A] + 7
+age[B] > age[A]
+age[B] > 100 && age[A] < 100
+Otherwise, A will friend request B.
+
+Note that if A requests B, B does not necessarily request A.  Also, people will not friend request themselves.
+
+How many total friend requests are made?
+
+Example 1:
+
+Input: [16,16]
+Output: 2
+Explanation: 2 people friend request each other.
+Example 2:
+
+Input: [16,17,18]
+Output: 2
+Explanation: Friend requests are made 17 -> 16, 18 -> 17.
+Example 3:
+
+Input: [20,30,100,110,120]
+Output: 3
+Explanation: Friend requests are made 110 -> 100, 120 -> 110, 120 -> 100.
+ 
+
+Notes:
+
+1 <= ages.length <= 20000.
+1 <= ages[i] <= 120.*/
+
+
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        vector<int> age(121,0);
+        for(int i = 0 ; i < ages.size() ; i++){
+            age[ages[i]]++;
+        }
+        int ans = 0;
+        for(int i = 0 ; i < ages.size() ; i++){
+            for(int j = 1 ; j < 121 ; j++){
+                if(ages[i] >= j and ages[i] < 2 * (j - 7)){
+                    if(!(ages[i] < 100 and j > 100)){
+                        if(ages[i] == j)
+                            ans = ans + age[j] - 1;
+                        else
+                            ans = ans + age[j];
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
