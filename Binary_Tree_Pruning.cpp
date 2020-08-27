0ff6e28f1d6a7b087d4af9ad8b6065994f979ff0
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-08-27 15:23:49 +0530

    Create Binary_Tree_Pruning.cpp

---
+++ Binary_Tree_Pruning.cpp
@@ -0,0 +1,66 @@
+/*We are given the head node root of a binary tree, where additionally every node's value is either a 0 or a 1.
+
+Return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+(Recall that the subtree of a node X is X, plus every node that is a descendant of X.)
+
+Example 1:
+Input: [1,null,0,0,1]
+Output: [1,null,0,null,1]
+ 
+Explanation: 
+Only the red nodes satisfy the property "every subtree not containing a 1".
+The diagram on the right represents the answer.
+
+
+Example 2:
+Input: [1,0,1,0,0,0,1]
+Output: [1,null,1,null,1]
+
+
+
+Example 3:
+Input: [1,1,0,1,1,0,1,0]
+Output: [1,1,0,1,1,null,1]
+
+
+
+Note:
+
+The binary tree will have at most 200 nodes.
+The value of each node will only be 0 or 1.*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int recur(TreeNode* root){
+        if(!root)
+            return 0;
+        int left = recur(root->left);
+        int right = recur(root->right);
+        if(left == 0)
+            root->left = NULL;
+        if(right == 0)
+            root->right = NULL;
+        return left + right + root->val;
+    }
+    TreeNode* pruneTree(TreeNode* root) {
+        if(!root)
+            return root;
+        recur(root);
+        if(!root->left and !root->right and root and root->val == 0)
+            root = NULL;
+        return root;
+    }
+};
