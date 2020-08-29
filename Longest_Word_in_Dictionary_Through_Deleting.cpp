6497c767b6c73a60496d494cb57bd540fd3b4565
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-08-29 21:26:19 +0530

    Create Longest_Word_in_Dictionary_Through_Deleting.cpp

---
+++ Longest_Word_in_Dictionary_Through_Deleting.cpp
@@ -0,0 +1,45 @@
+/*Given a string and a string dictionary, find the longest string in the dictionary that can be formed by deleting some characters of the given string. If there are more than one possible results, return the longest word with the smallest lexicographical order. If there is no possible result, return the empty string.
+
+Example 1:
+Input:
+s = "abpcplea", d = ["ale","apple","monkey","plea"]
+
+Output: 
+"apple"
+Example 2:
+Input:
+s = "abpcplea", d = ["a","b","c"]
+
+Output: 
+"a"
+Note:
+All the strings in the input will only contain lower-case letters.
+The size of the dictionary won't exceed 1,000.
+The length of all the strings in the input won't exceed 1,000.*/
+
+
+class Solution {
+public:
+    bool subsequence(string a , string b){
+        if(a.size() > b.size())
+            return false;
+        int j = 0;
+        for(int i = 0 ; i < b.size() and j < a.size() ; i++){
+            if(a[j] == b[i])
+                j++;
+        }
+        return j >= a.size();
+    }
+    string findLongestWord(string s, vector<string>& d) {
+        string ans = "";
+        for(int i = 0 ; i < d.size() ; i++){
+            if(subsequence(d[i],s)){
+                if(d[i].size() > ans.size())
+                    ans = d[i];
+                else if(d[i].size() == ans.size())
+                    ans = min(ans,d[i]);
+            }
+        }
+        return ans;
+    }
+};
