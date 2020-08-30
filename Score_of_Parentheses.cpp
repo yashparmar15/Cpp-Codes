38ec7325a8f2bf6d87d86950ee0b845b4dffcd6c
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-08-30 22:11:53 +0530

    Create Score_of_Parentheses.cpp

---
+++ Score_of_Parentheses.cpp
@@ -0,0 +1,48 @@
+/*Given a balanced parentheses string S, compute the score of the string based on the following rule:
+
+() has score 1
+AB has score A + B, where A and B are balanced parentheses strings.
+(A) has score 2 * A, where A is a balanced parentheses string.
+ 
+
+Example 1:
+
+Input: "()"
+Output: 1
+Example 2:
+
+Input: "(())"
+Output: 2
+Example 3:
+
+Input: "()()"
+Output: 2
+Example 4:
+
+Input: "(()(()))"
+Output: 6
+ 
+
+Note:
+
+S is a balanced parentheses string, containing only ( and ).
+2 <= S.length <= 50*/
+
+
+class Solution {
+public:
+    int scoreOfParentheses(string S) {
+        stack<int> stack;
+        int cur = 0;
+        for (char i : S)
+            if (i == '(') {
+                stack.push(cur);
+                cur = 0;
+            }
+            else {
+                cur += stack.top() + max(cur, 1);
+                stack.pop();
+            }
+        return cur;
+    }
+};
