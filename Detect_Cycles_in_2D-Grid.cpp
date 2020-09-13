53ecab165d89ca5b1c0baf97689b49ce4c535b73
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-09-13 21:12:18 +0530

    Update Detect_Cycles_in_2D-Grid.cpp

--- Detect_Cycles_in_2D-Grid.cpp
+++ Detect_Cycles_in_2D-Grid.cpp
@@ -68,3 +68,45 @@ public:
         return 0;
     }
 };
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+class Solution {
+public:
+    int ch;
+    bool dfs(vector<vector<pair<int,int>>> &v,int i,int j,int count,int index){
+        if(i < 0 or j < 0 or i >= v.size() or j >= v[0].size() or v[i][j].first != ch) return 0;
+        if(v[i][j].second){
+            if(abs(v[i][j].second - count) > 3) return 1;
+            else return 0;
+        }
+        v[i][j].second = count;
+        if(index == 0) return dfs(v,i+1,j,count + 1,1) or dfs(v,i,j+1,count + 1,3) or dfs(v,i,j-1,count + 1,4) or dfs(v,i-1,j,count + 1,2);
+        else if(index == 1) return dfs(v,i+1,j,count + 1,1) or dfs(v,i,j+1,count + 1,3) or dfs(v,i,j-1,count + 1,4);
+        else if(index == 2) return dfs(v,i-1,j,count + 1,2) or dfs(v,i,j+1,count + 1,3) or dfs(v,i,j-1,count + 1,4);
+        else if(index == 3) return dfs(v,i+1,j,count + 1,1) or dfs(v,i-1,j,count + 1,2) or dfs(v,i,j+1,count + 1,3);
+        else return dfs(v,i+1,j,count + 1,1) or dfs(v,i-1,j,count + 1,2) or dfs(v,i,j-1,count + 1,4);
+    }
+    bool containsCycle(vector<vector<char>>& grid) {
+        vector<vector<pair<int,int>>> v(grid.size(),vector<pair<int,int>> (grid[0].size(),make_pair(0,0)));
+        vector<int> H(26,0);
+        for(int i = 0 ; i < grid.size() ; i++)
+            for(int j = 0 ; j < grid[0].size() ; j++){
+                v[i][j].first = int(grid[i][j]);
+                H[v[i][j].first - 97]++;
+            }
+        for(int i = 0 ; i < grid.size() ; i++)
+            for(int j = 0 ; j < grid[0].size() ; j++){
+                ch = v[i][j].first;
+                if(v[i][j].second == 0 and H[ch - 97] >= 4){
+                    if(dfs(v,i,j,1,0))
+                        return 1;
+                }
+            }
+        return 0;
+    }
+};
+
