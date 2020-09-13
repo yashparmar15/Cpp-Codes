c208f01fc15268997df6807f20fe32f6bc0420b5
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-09-13 16:37:30 +0530

    Create Reconstruct_a_2_Row_Binary_Matrix.cpp

---
+++ Reconstruct_a_2_Row_Binary_Matrix.cpp
@@ -0,0 +1,65 @@
+/*Given the following details of a matrix with n columns and 2 rows :
+
+The matrix is a binary matrix, which means each element in the matrix can be 0 or 1.
+The sum of elements of the 0-th(upper) row is given as upper.
+The sum of elements of the 1-st(lower) row is given as lower.
+The sum of elements in the i-th column(0-indexed) is colsum[i], where colsum is given as an integer array with length n.
+Your task is to reconstruct the matrix with upper, lower and colsum.
+
+Return it as a 2-D integer array.
+
+If there are more than one valid solution, any of them will be accepted.
+
+If no valid solution exists, return an empty 2-D array.
+
+ 
+
+Example 1:
+
+Input: upper = 2, lower = 1, colsum = [1,1,1]
+Output: [[1,1,0],[0,0,1]]
+Explanation: [[1,0,1],[0,1,0]], and [[0,1,1],[1,0,0]] are also correct answers.
+Example 2:
+
+Input: upper = 2, lower = 3, colsum = [2,2,1,1]
+Output: []
+Example 3:
+
+Input: upper = 5, lower = 5, colsum = [2,1,2,0,1,0,1,2,0,1]
+Output: [[1,1,1,0,1,0,0,1,0,0],[1,0,1,0,0,0,1,1,0,1]]
+ 
+
+Constraints:
+
+1 <= colsum.length <= 10^5
+0 <= upper, lower <= colsum.length
+0 <= colsum[i] <= 2*/
+
+
+class Solution {
+public:
+    vector<vector<int>> reconstructMatrix(int upper, int lower, vector<int>& colsum) {
+        vector<vector<int>> ans(2,vector<int>(colsum.size(),0));
+        int N = colsum.size();
+        int sum = 0;
+        for(int i = 0 ; i < N ; i++) sum += colsum[i];
+        if(upper > N or lower > N or upper + lower != sum) return{};
+        for(int i = 0 ; i < N and upper >= 0 and lower >= 0 ; i++){
+            if(colsum[i] == 2){
+                ans[0][i] = 1;
+                ans[1][i] = 1;
+                lower--;
+                upper--;
+            } 
+        }
+        for(int i = 0 ; i < N and upper >= 0 and lower >= 0 ; i++){
+            if(colsum[i] == 1){
+                if(upper) ans[0][i] = 1,upper--;
+                else if(lower) ans[1][i] = 1,lower--;
+                else return {};
+            }
+        }
+        if(upper == 0 and lower == 0) return ans;
+        return {};
+    }
+};
