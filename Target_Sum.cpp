9bf874e6a7ea67bdd4d776351f7f7c8add0a49e9
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-09-25 13:22:28 +0530

    Update Target_Sum.cpp

--- Target_Sum.cpp
+++ Target_Sum.cpp
@@ -41,3 +41,46 @@ public:
         return ans;
     }
 };
+
+
+/////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int ans = 0;
+    vector<vector<int>> M;
+    int dfs(vector<int> &nums,int sum,int &S,int index){
+        if(sum == S and index == nums.size())
+            return 1;
+        if(index == nums.size()) return 0;
+        if(M[index][20000 - sum]) return M[index][20000 - sum];
+        M[index][20000 - sum] = dfs(nums,sum + nums[index],S,index + 1) + dfs(nums,sum - nums[index],S,index + 1);  
+        return M[index][20000 - sum];
+    }
+    int findTargetSumWays(vector<int>& nums, int S) {
+        M.resize(20,vector<int>(40000,0));
+        if(nums.size() == 0) return 0;
+        ans += dfs(nums,nums[0],S,1);
+        ans += dfs(nums,-nums[0],S,1);
+        return ans;
+    }
+};
+
+
+// class Solution {
+// public:
+//     int ans = 0;
+//     void dfs(vector<int> &nums,int sum,int &S,int index){
+//         if(sum == S and index == nums.size())
+//             ans++;
+//         if(index == nums.size()) return;
+//         dfs(nums,sum + nums[index],S,index + 1);
+//         dfs(nums,sum - nums[index],S,index + 1);    
+//     }
+//     int findTargetSumWays(vector<int>& nums, int S) {
+//         if(nums.size() == 0) return 0;
+//         dfs(nums,nums[0],S,1);
+//         dfs(nums,-nums[0],S,1);
+//         return ans;
+//     }
+// };
