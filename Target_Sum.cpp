c4a4b5fcaadccbb1a41810b74b99912daf7a4c29
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-09-25 13:06:10 +0530

    Create Target_Sum.cpp

---
+++ Target_Sum.cpp
@@ -0,0 +1,43 @@
+/*You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+
+Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+Example 1:
+
+Input: nums is [1, 1, 1, 1, 1], S is 3. 
+Output: 5
+Explanation: 
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+
+There are 5 ways to assign symbols to make the sum of nums be target 3.
+ 
+
+Constraints:
+
+The length of the given array is positive and will not exceed 20.
+The sum of elements in the given array will not exceed 1000.
+Your output answer is guaranteed to be fitted in a 32-bit integer.*/
+
+
+class Solution {
+public:
+    int ans = 0;
+    void dfs(vector<int> &nums,int sum,int &S,int index){
+        if(sum == S and index == nums.size())
+            ans++;
+        if(index == nums.size()) return;
+        dfs(nums,sum + nums[index],S,index + 1);
+        dfs(nums,sum - nums[index],S,index + 1);    
+    }
+    int findTargetSumWays(vector<int>& nums, int S) {
+        if(nums.size() == 0) return 0;
+        dfs(nums,nums[0],S,1);
+        dfs(nums,-nums[0],S,1);
+        return ans;
+    }
+};
