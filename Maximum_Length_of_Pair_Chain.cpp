b693d1882e376de661676c548beafeaff0fe3bbb
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-10-02 22:38:20 +0530

    Update Maximum_Length_of_Pair_Chain.cpp

--- Maximum_Length_of_Pair_Chain.cpp
+++ Maximum_Length_of_Pair_Chain.cpp
@@ -33,3 +33,26 @@ public:
         return ans;
     }
 };
+
+
+////////////////////////////
+
+
+class Solution {
+public:
+    static bool comp(vector<int> a,vector<int> b){
+        return a[1] < b[1];
+    }
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(),pairs.end(),comp);
+        int ans = 0;
+        int cur = INT_MIN;
+        for(int i = 0 ; i < pairs.size() ; i++){
+            if(cur < pairs[i][0]){
+                cur = pairs[i][1];
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
