2bfa8d2768435a7f5544a943fdb09c31be93aa02
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-10-23 23:58:48 +0530

    Create Maximum_Gap.cpp

---
+++ Maximum_Gap.cpp
@@ -0,0 +1,32 @@
+/*Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+
+Return 0 if the array contains less than 2 elements.
+
+Example 1:
+
+Input: [3,6,9,1]
+Output: 3
+Explanation: The sorted form of the array is [1,3,6,9], either
+             (3,6) or (6,9) has the maximum difference 3.
+Example 2:
+
+Input: [10]
+Output: 0
+Explanation: The array contains less than 2 elements, therefore return 0.
+Note:
+
+You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+Try to solve it in linear time/space.*/
+
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size() < 2) return 0;
+        sort(nums.begin(),nums.end());
+        int ans = 0;
+        for(int i = 1 ; i < nums.size() ; i++)
+            ans = max(ans,nums[i] - nums[i - 1]);
+        return ans;
+    }
+};
