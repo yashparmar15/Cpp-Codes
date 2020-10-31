4621d72d6264bb394ae9ecec440a176ee74c1dd1
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-10-31 21:18:29 +0530

    Create Sort_Array_By_Increasing_Frequency.cpp

---
+++ Sort_Array_By_Increasing_Frequency.cpp
@@ -0,0 +1,50 @@
+/*Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+Return the sorted array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,2,2,3]
+Output: [3,1,1,2,2,2]
+Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+Example 2:
+
+Input: nums = [2,3,1,3,2]
+Output: [1,3,3,2,2]
+Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+Example 3:
+
+Input: nums = [-1,1,-6,4,5,-6,1,4,1]
+Output: [5,-1,4,4,-6,-6,1,1,1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100*/
+
+class Solution {
+public:
+    static bool comp(pair<int,int> a,pair<int,int> b){
+        if(a.first == b.first) return a.second > b.second;
+        return a.first < b.first;
+    }
+    vector<int> frequencySort(vector<int>& nums) {
+        vector<pair<int,int>> v;
+        unordered_map<int,int> M;
+        for(auto n : nums){
+            M[n]++;
+        }
+        for(int i = 0 ; i < nums.size() ; i++){
+            v.push_back(make_pair(M[nums[i]],nums[i]));
+        }
+        sort(v.begin(),v.end(),comp);
+        vector<int> ans;
+        for(int i = 0 ; i < v.size() ; i++){
+            ans.push_back(v[i].second);
+        }
+        return ans;
+    }
+};
