9bee5b83d3b03664f5a504512585c1401bbd164e
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-10-31 15:28:39 +0530

    Create Range_Sum_Query_2D_Immutable.cpp

---
+++ Range_Sum_Query_2D_Immutable.cpp
@@ -0,0 +1,48 @@
+/*Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+Range Sum Query 2D
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
+
+Example:
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+Note:
+You may assume that the matrix does not change.
+There are many calls to sumRegion function.
+You may assume that row1 ≤ row2 and col1 ≤ col2.*/
+
+
+class NumMatrix {
+public:
+    vector<vector<int>> v,v1;
+    NumMatrix(vector<vector<int>>& matrix) {
+        this->v = matrix;
+        this->v1 = matrix;
+        for(int i = 0 ; i < v.size() ; i++)
+            for(int j = 1 ; j < v[i].size() ; j++)
+                v[i][j] += v[i][j - 1];
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int ans = 0;
+        for(int i = row1 ; i <= row2 ; i++){
+            ans += v[i][col2] - v[i][col1] + v1[i][col1];
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
