2c00790b5d09cb7b0dc5e54106101b88cb1a6ea3
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-11-01 17:31:00 +0530

    Create Split_Array_into_Fibonacci_Sequence.cpp

---
+++ Split_Array_into_Fibonacci_Sequence.cpp
@@ -0,0 +1,95 @@
+/*Given a string S of digits, such as S = "123456579", we can split it into a Fibonacci-like sequence [123, 456, 579].
+
+Formally, a Fibonacci-like sequence is a list F of non-negative integers such that:
+
+0 <= F[i] <= 2^31 - 1, (that is, each integer fits a 32-bit signed integer type);
+F.length >= 3;
+and F[i] + F[i+1] = F[i+2] for all 0 <= i < F.length - 2.
+Also, note that when splitting the string into pieces, each piece must not have extra leading zeroes, except if the piece is the number 0 itself.
+
+Return any Fibonacci-like sequence split from S, or return [] if it cannot be done.
+
+Example 1:
+
+Input: "123456579"
+Output: [123,456,579]
+Example 2:
+
+Input: "11235813"
+Output: [1,1,2,3,5,8,13]
+Example 3:
+
+Input: "112358130"
+Output: []
+Explanation: The task is impossible.
+Example 4:
+
+Input: "0123"
+Output: []
+Explanation: Leading zeroes are not allowed, so "01", "2", "3" is not valid.
+Example 5:
+
+Input: "1101111"
+Output: [110, 1, 111]
+Explanation: The output [11, 0, 11, 11] would also be accepted.
+Note:
+
+1 <= S.length <= 200
+S contains only digits.*/
+
+class Solution {
+public:
+    vector<int> ans;
+    bool check = false;
+    bool zero = false;
+    void backtrack(vector<int> temp,string &S,int index){
+        if(check) return;
+        if(index == S.size() and temp.size() >= 3) {
+            for(int i = 0 ; i < temp.size() ; i++) {
+                if(temp[i] < 0) return;
+            }
+            ans = temp;
+            check = true;
+            return;
+        }
+        if(temp.size() == 1){
+            long long int num = 0;
+            for(int i = index ; i < S.size() ; i++){
+                num = num * 10 + int(S[i]) - 48;
+                if(num > 10000000000) break;
+                if(num == 0 and i == index + 1) break;
+                temp.push_back(num);
+                backtrack(temp,S,i + 1);
+                temp.pop_back();
+            }
+        } else {
+            long long int num = 0;
+            for(int i = index ; i < S.size() ; i++){
+                num = num * 10 + int(S[i]) - 48;
+                if(num > 10000000000) break;
+                if(num == 0 and i == index + 1) break;
+                if(num == (long long)temp[temp.size() - 1] + (long long)temp[temp.size() - 2]){
+                    temp.push_back(num);
+                    backtrack(temp,S,i + 1);
+                    temp.pop_back();
+                }
+            }
+        }
+    }
+    vector<int> splitIntoFibonacci(string S) {
+        vector<int> temp;
+        long long int num = 0;
+        if(S.size() == 0) return {};
+        for(int i = 0 ; i < S.size() ; i++){
+            num = num * 10 + int(S[i]) - 48;
+            if(num > 10000000000) break;
+            temp.push_back(num);
+            backtrack(temp,S,i + 1);
+            temp.pop_back();
+            if(num == 0) break;
+        }
+        vector<int> res;
+        if(ans.size() >= 3 and check) return ans;
+        return {};
+    }
+};
