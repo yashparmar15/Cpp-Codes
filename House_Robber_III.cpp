9920e0db10ba968c36688030d60a4ff439177eb2
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-11-23 18:43:02 +0530

    Update House_Robber_III.cpp

--- House_Robber_III.cpp
+++ House_Robber_III.cpp
@@ -57,3 +57,41 @@ public:
         return max(res[0],res[1]);
     }
 };
+
+
+
+////////////////////////////////////////////////////////
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<TreeNode*,unordered_map<int,int>> M;
+    int dfs(TreeNode* root,int picked){
+        if(!root) return 0;
+        if(M[root][picked]) return M[root][picked];
+        int temp = 0;
+        if(picked)
+            temp = max(temp,dfs(root->right,0) + dfs(root->left,0));
+        else {
+            temp = max(temp,dfs(root->right,1) + dfs(root->left,1) + root->val);
+            temp = max(temp,dfs(root->right,0) + dfs(root->left,0));
+        }
+        return M[root][picked] = temp;
+    }
+    int rob(TreeNode* root) {
+        if(!root) return 0;
+        return dfs(root,0);
+    }
+};
