2b97b27826f39608c10cb39f0351bbde1b66ce42
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-12-11 01:09:13 +0530

    Create Rotate_Digits.cpp

---
+++ Rotate_Digits.cpp
@@ -0,0 +1,42 @@
+/*X is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from X.  Each digit must be rotated - we cannot choose to leave it alone.
+
+A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other (on this case they are rotated in a different direction, in other words 2 or 5 gets mirrored); 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
+
+Now given a positive number N, how many numbers X from 1 to N are good?
+
+Example:
+Input: 10
+Output: 4
+Explanation: 
+There are four good numbers in the range [1, 10] : 2, 5, 6, 9.
+Note that 1 and 10 are not good numbers, since they remain unchanged after rotating.
+Note:
+
+N  will be in range [1, 10000].*/
+
+
+class Solution {
+public:
+    int rotatedDigits(int N) {
+        int count=0;
+		for(int i=1;i<=N;i++)
+		{
+			int temp=i,flag=0;
+			while(temp>0)
+			{
+				int j=temp%10;
+				if(j==2||j==5||j==6||j==9)
+					flag=1;
+				else if(j==3||j==4||j==7)
+				{
+					flag=0;
+					break;
+				}
+				temp=temp/10;
+			}
+			if(flag==1)
+				count++;
+		}
+		return count;
+    }
+};
