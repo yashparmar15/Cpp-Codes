056842b2bf64f891ae52d59b68117005522adbe2
Yash Parmar <39482051+yashparmar15@users.noreply.github.com>
2020-12-31 15:55:15 +0530

    Create Maximum_Heights_By_Stacking_Cuboids.cpp

---
+++ Maximum_Heights_By_Stacking_Cuboids.cpp
@@ -0,0 +1,73 @@
+/*Given n cuboids where the dimensions of the ith cuboid is cuboids[i] = [widthi, lengthi, heighti] (0-indexed). Choose a subset of cuboids and place them on each other.
+
+You can place cuboid i on cuboid j if widthi <= widthj and lengthi <= lengthj and heighti <= heightj. You can rearrange any cuboid's dimensions by rotating it to put it on another cuboid.
+
+Return the maximum height of the stacked cuboids.
+
+ 
+
+Example 1:
+
+
+
+Input: cuboids = [[50,45,20],[95,37,53],[45,23,12]]
+Output: 190
+Explanation:
+Cuboid 1 is placed on the bottom with the 53x37 side facing down with height 95.
+Cuboid 0 is placed next with the 45x20 side facing down with height 50.
+Cuboid 2 is placed next with the 23x12 side facing down with height 45.
+The total height is 95 + 50 + 45 = 190.
+Example 2:
+
+Input: cuboids = [[38,25,45],[76,35,3]]
+Output: 76
+Explanation:
+You can't place any of the cuboids on the other.
+We choose cuboid 1 and rotate it so that the 35x3 side is facing down and its height is 76.
+Example 3:
+
+Input: cuboids = [[7,11,17],[7,17,11],[11,7,17],[11,17,7],[17,7,11],[17,11,7]]
+Output: 102
+Explanation:
+After rearranging the cuboids, you can see that all cuboids have the same dimension.
+You can place the 11x7 side down on all cuboids so their heights are 17.
+The maximum height of stacked cuboids is 6 * 17 = 102.
+ 
+
+Constraints:
+
+n == cuboids.length
+1 <= n <= 100
+1 <= widthi, lengthi, heighti <= 100*/
+
+
+
+class Solution {
+public:
+    static bool comp(vector<int> a,vector<int> b){
+        if(a[2] < b[2]) return 1;
+        if(a[2] > b[2]) return 0;
+        if(a[1] < b[1]) return 1;
+        if(a[1] > b[1]) return 0;
+        if(a[0] < b[0]) return 1;
+        if(a[0] > b[0]) return 0;
+        return 0;
+    }
+    int dp[101][101][101];
+    int dfs(vector<vector<int>> &v,int index,int pL,int pW){
+        if(index == v.size()) return 0;
+        if(dp[index][pL][pW] != -1) return dp[index][pL][pW];
+        int temp = INT_MIN;
+        temp = max(temp,dfs(v,index + 1,pL,pW));
+        if(v[index][0] >= pL and v[index][1] >= pW)
+            temp = max(temp,dfs(v,index + 1,v[index][0],v[index][1]) + v[index][2]);
+        return dp[index][pL][pW] = temp;
+    }
+    int maxHeight(vector<vector<int>>& v) {
+        memset(dp,-1,sizeof(dp));
+        for(int i = 0 ; i < v.size() ; i++)
+            sort(v[i].begin(),v[i].end());
+        sort(v.begin(),v.end(),comp);
+        return dfs(v,0,0,0);
+    }
+};
